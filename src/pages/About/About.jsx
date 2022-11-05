8f253cd1960eb61a80afdb6d261280bf91abda8e
Abdelkrim31 <103307521+Abdelkrim31@users.noreply.github.com>
2022-11-05 22:10:19 +0100

    Collapse About

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import AboutBanner from "../../components/AboutBanner/AboutBanner";
-
-
-
+import Collapse from "../../components/Collapse/Collapse"
+import aboutArray from "../../datas/aboutArray.json"  // j'ai créé un fichier JSON avec les données des collapses
 
 
 export default function About() {
 	return (
 		<div>
 			<AboutBanner />
-
+			{aboutArray.map((rule, id) => (
+				<Collapse key={id} aboutTitle={rule.aboutTitle} aboutText={rule.aboutText} aboutStyle="about-style" />
+			))}
 		</div>
 	);
 }
