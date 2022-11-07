4f1489342a565c14a8ef748588227a8c09b1a37b
Abdelkrim31 <103307521+Abdelkrim31@users.noreply.github.com>
2022-11-07 21:49:47 +0100

    Début implément tags

---
+++ src/components/Tag/Tag.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../../style/main.scss";
+
+export default function Tag(props) {
+  return (
+		<div >
+			<span >{props.tag}</span>
+		</div>
+	);
+}
--- src/pages/FicheLogement/FicheLogement.jsx
+++ src/pages/FicheLogement/FicheLogement.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import Carrousel from "../../components/Carrousel/Carrousel";
+import Tag from "../../components/Tag/Tag";
 import data from "../../datas/logements.json";
 
 
@@ -8,12 +9,22 @@ export default function FicheLogement() {
 	const params = useParams();
 	const pickedAppart = data.find(({ id }) => id === params.id);
 	const slidePics = pickedAppart.pictures;
+	const tags = pickedAppart.tags;
 
 	console.log(pickedAppart);
 	if(pickedAppart !== undefined) {
 	return (
-		<div key={params.id} className="fiche-container">
+		<div key={params.id} >
 			<Carrousel slides={slidePics} />
+			<section>
+				<div>
+					<div>
+						{tags.map((tag) => (
+							<Tag key={tag} tag={tag} />
+						))}
+					</div>
+				</div>
+			</section>
 		</div>
 	);
 	}	
