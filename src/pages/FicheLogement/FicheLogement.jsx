ad4429898e519ed76bba7f32a1fba9d4711710d1
Abdelkrim31 <103307521+Abdelkrim31@users.noreply.github.com>
2022-11-07 17:38:43 +0100

    Suite Carrousel

--- src/pages/FicheLogement/FicheLogement.jsx
+++ src/pages/FicheLogement/FicheLogement.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import Carrousel from "../../components/Carrousel/Carrousel";
+import data from "../../datas/logements.json";
 
 
 export default function FicheLogement() {
+	const params = useParams();
+	const pickedAppart = data.find(({ id }) => id === params.id);
+	const slidePics = pickedAppart.pictures;
+
+	console.log(pickedAppart);
+	if(pickedAppart !== undefined) {
 	return (
-		<div>
-		<h1>voici la fiche Logement de l'id :</h1>
-		< Carrousel />
-	    </div>
+		<div key={params.id} className="fiche-container">
+			<Carrousel slides={slidePics} />
+		</div>
 	);
+	}	
 }
 
 
