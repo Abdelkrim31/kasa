736e9d35411a48e4a36a3a26f9b1338a821941b7
Abdelkrim31 <103307521+Abdelkrim31@users.noreply.github.com>
2022-11-07 16:27:31 +0100

    début Carroussel

---
+++ src/components/Carrousel/Carrousel.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export default function Carrousel() {
+    return (
+        <div>
+          <h1>Carrousel</h1>
+        </div>
+      )
+	
+}
--- src/pages/FicheLogement/FicheLogement.jsx
+++ src/pages/FicheLogement/FicheLogement.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Carrousel from "../../components/Carrousel/Carrousel";
+
 
 export default function FicheLogement() {
 	return (
 		<div>
-			<h1>Logement</h1>
-		</div>
+		<h1>voici la fiche Logement de l'id :</h1>
+		< Carrousel />
+	    </div>
 	);
 }
+
+
