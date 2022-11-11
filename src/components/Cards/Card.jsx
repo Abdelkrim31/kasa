3554e9133881751c372349b881bf0a3bd681f008
Abdelkrim31 <103307521+Abdelkrim31@users.noreply.github.com>
2022-11-11 22:57:16 +0100

    Responsive HomePage

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "../../style/main.scss";
-
-export default function Card({ cover, title }) {
+export default function Card({ cover, title }) { //les cards de locations récupèrent les couvertures et titres en props depuis Home
 	return (
 		<article className="card-logement">
 			<img src={cover} alt="location" />
